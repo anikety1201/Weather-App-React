042e9938e143258de9d1ad904085a70f7cf94453
Aniket Yadav <73031565+anikety1201@users.noreply.github.com>
2022-03-01 15:24:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import React from "react";
-// import Resturant from "./component/basics/Resturant";
-// import UseState from "./component/Hooks/useState";
-// import UseEffect from "./component/Hooks/useEffect";
-// import UseReducer from "./component/Hooks/useReducer";
-// import Todo from "./component/todoReact/todo";
 import Temp from "./component/weather/temp";
 
 const App = () => {
